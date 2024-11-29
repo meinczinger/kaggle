b3d8f6cd2cd438094076a879e14df35e487590d9
Tibor Meinczinger <meinczinger@gmail.com>
2024-11-30 00:10:02 +0100

    main

--- connectX/main.py
+++ connectX/main.py
@@ -2,20 +2,22 @@ import sys
 import os
 
 cwd = "/kaggle_simulations/agent/"
-print("Before if")
 if os.path.exists(cwd):
-    print("Installing...")
+    # print(os.getcwd())
+    # print(os.listdir())
+    # print(os.listdir("/kaggle_simulations/agent/resources/"))
+    # os.symlink(
+    #     "/kaggle_simulations/agent/resources/", "./resoures", target_is_directory=True
+    # )
+    # print(os.listdir("resources"))
+    # print(os.listdir("resources\models"))
     sys.path.append(cwd)
-    sys.path.append(cwd + "agents/")
-    sys.path.append(cwd + "agents/mcts/")
-    sys.path.append(cwd + "agents/game/")
-    sys.path.append(cwd + "agents/model/")
-    sys.path.append(cwd + "agents/agent/")
-    os.system("pip install game/")
-    os.system("pip install model/")
-    os.system("pip install mcts/")
-    os.system("pip install agent/")
-    os.system("pip freeze")
+    sys.path.append(cwd + "mcts/")
+    sys.path.append(cwd + "game/")
+    sys.path.append(cwd + "game/board/")
+    sys.path.append(cwd + "model/")
+    sys.path.append(cwd + "agent/")
+    sys.path.append(cwd + "logger/")
 else:
     cwd = ""
 
@@ -25,5 +27,5 @@ from agent.mcts_agent import MCTSAgent
 
 def act(observation, configuration):
     return MCTSAgent.get_instance(
-        configuration, NeuralNetworkMonteCarloTreeSearch(configuration)
+        configuration, NeuralNetworkMonteCarloTreeSearch.get_instance(configuration)
     ).act(observation)
